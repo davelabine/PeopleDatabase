043af7d48e37afae1b2489daf4ded1752ef78861
davelabine <davidl@smarttech.com>
2015-11-16 10:41:16 -0800

    Initial Commit

---
+++ PeopleDatabase/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PeopleDatabase
+//
+//  Created by David LaBine on 11/16/15.
+//  Copyright © 2015 David LaBine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
